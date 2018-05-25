5697142e3a829082ce47d4f07ddc55f9f828212a
Prajjwal <taoprajjwal@gmail.com>
2018-05-25 15:45:55 +0545

    Update optimize.py

--- src/optimize.py
+++ src/optimize.py
@@ -14,7 +14,7 @@ DEVICES = 'CUDA_VISIBLE_DEVICES'
 def optimize(content_targets, style_target, content_weight, style_weight,
              tv_weight, vgg_path, epochs=2, print_iterations=1000,
              batch_size=4, save_path='saver/fns.ckpt', slow=False,
-             learning_rate=1e-3, debug=False):
+             learning_rate=1e-3, debug=True):
     if slow:
         batch_size = 1
     mod = len(content_targets) % batch_size
@@ -111,7 +111,7 @@ def optimize(content_targets, style_target, content_weight, style_weight,
                 feed_dict = {
                    X_content:X_batch
                 }
-
+                print("start training)
                 train_step.run(feed_dict=feed_dict)
                 end_time = time.time()
                 delta_time = end_time - start_time
